586885f3332c3a890afc53e1dd3de86a6a836758
Davina-dev <davinamr91@gmail.com>
2022-05-25 16:01:33 +0200

    docs: create program object

--- lib/src/main/kotlin/sample/Lavadora.kt
+++ lib/src/main/kotlin/sample/Lavadora.kt
@@ -1,9 +1,3 @@
-
-
-import MiLavadora
-import CajonDetergenteSuavizante
-import EnchufeTomaCorriente
-
 object MiLavadora {
     var marca: String = "Miele"
     var modelo: String = "WCA020 WCS Active"
@@ -12,74 +6,65 @@ object MiLavadora {
     var isActive: Boolean = false
     var detergente: Boolean = false
     var suavizante: Boolean = false
+    var isOn: Boolean = false
 
-    fun introducirRopa(){
-        if(this.capacidad <= 0 ){
-            println("Tambor lleno,  isFull: ${this.isFull}") 
-        }else{
-            MiLavadora.capacidad = this.capacidad -1
+    fun introducirRopa() {
+        if (this.capacidad <= 0) {
+            println("Tambor lleno,  isFull: ${this.isFull}")
+        } else {
+            MiLavadora.capacidad = this.capacidad - 1
             println("introduciendo ropa : ${MiLavadora.capacidad}")
-        } 
-    }
-
-    fun sacarRopa(){
-        if(this.capacidad <= 0 || this.capacidad >7){
-            println("Error, no es posible sacar ropa") 
-        }else{
-            MiLavadora.capacidad = this.capacidad +1
-            println("sacando ropa: ${MiLavadora.capacidad}")
         }
     }
 
+    fun sacarRopa() {
+        if (this.capacidad <= 0 || this.capacidad > 7) {
+            println("Error, no es posible sacar ropa")
+        } else {
+            MiLavadora.capacidad = this.capacidad + 1
+            println("sacando ropa: ${MiLavadora.capacidad}")
+        }
+    }
+}
 
- }
-
- object EnchufeTomaCorriente {
+object EnchufeTomaCorriente {
     var encendida: Boolean = false
-    fun on(){
-       MiLavadora.isActive = true
+    fun on() {
+        MiLavadora.isOn = true
     }
-    fun off(){
-        MiLavadora.isActive = false
+    fun off() {
+        MiLavadora.isOn = false
     }
- }
-    
+}
 
-object CajonDetergenteSuavizante{
-    fun agregarSuavizante(){
+object CajonDetergenteSuavizante {
+    fun agregarSuavizante() {
         MiLavadora.suavizante = true
     }
-    fun agregarDetergente(){
+    fun agregarDetergente() {
         MiLavadora.detergente = true
     }
 }
 
-// object ProgramaLavadora(programa?){
-//     var progRapido : Boolean = false
-//     var progNormal : Boolean = false
-//     var progLento : Boolean = false
-//     val list = listOf("rapido", "normal", "lento")?
-//     fun GetPrograma(){
-//       
-//        }
-//   }
-//     }
-//iniciarPrograma(){
-//    when
-//}
-//detenerPrograma(){
-//    when
-//}
+object ProgramaLavadora {
+    //     var progRapido : Boolean = false
+    //     var progNormal : Boolean = false
+    //     var progLento : Boolean = false
+    //     val list = listOf("rapido", "normal", "lento")?
+    //     fun GetPrograma(x){
 
+    fun iniciarPrograma() {
+        MiLavadora.isActive = true
+    }
 
+    fun detenerPrograma() {
+        MiLavadora.isActive = false
+    }
+}
 
-fun main(){
+fun main() {
     MiLavadora.introducirRopa()
     MiLavadora.sacarRopa()
-   
-    
-    
-    
     CajonDetergenteSuavizante.agregarDetergente()
     CajonDetergenteSuavizante.agregarSuavizante()
     println("¿puse detergente?: ${MiLavadora.detergente}")
